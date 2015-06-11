1bacfd599c405e729a1e21d8cad7fd5a17d7b626
Dmitrii <wild_dima@mail.ru>
2015-06-11 16:22:37 +0300

    Добавил все фильтры, добавил возможность изменять свою вебстудию

--- app/controllers/webstudios_controller.rb
+++ app/controllers/webstudios_controller.rb
@@ -1,18 +1,29 @@
 class WebstudiosController < ApplicationController
   def index
+    @c = City.all
   	@ws=Webstudio.all
   end
 
   def filter
+    @c = City.all
     @ws = Array.new
-    Webstudio.all.each do |ws|
-      if ws.average_ticket.between?(params[:filter][:pricelow].to_f,params[:filter][:pricehigh].to_f)
-        @ws << ws
+    if (!params[:filter][:pricelow].empty?&&!params[:filter][:pricehigh].empty?&&!params[:filter][:city].empty?) == true
+      Webstudio.all.each do |ws|
+           @ws << ws if ws.average_ticket.to_f.between?(params[:filter][:pricelow].to_f,params[:filter][:pricehigh].to_f)&&ws.city_id.to_i==params[:filter][:city].to_i
+      end
+    elsif (!params[:filter][:pricelow].empty?&&!params[:filter][:pricehigh].empty?) == true
+      Webstudio.all.each do |ws|
+           @ws << ws if ws.average_ticket.to_f.between?(params[:filter][:pricelow].to_f,params[:filter][:pricehigh].to_f)
+      end
+    elsif (!params[:filter][:city].empty?) == true
+      Webstudio.all.each do |ws|
+           @ws << ws if ws.city_id.to_i==params[:filter][:city].to_i
+      end
+    else 
+      Webstudio.all.each do |ws|
+          @ws << ws
       end
     end
-    # params[:filter][:pricelow]
-    # params[:filter][:pricehigh]
-    # @ws=Webstudio.where(average_ticket: ()
   end
 
   def show
@@ -37,9 +48,4 @@ class WebstudiosController < ApplicationController
   def webstudio_params
   	params.require(:webstudio).permit(:name, :average_ticket, :description, :img)
   end
-
-
-  # def filter_params
-  #   params.require(:filter).permit(:pricelow, :pricehigh)
-  # end
 end
